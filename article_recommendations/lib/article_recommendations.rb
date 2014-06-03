c74502813dc383bf4592a8262bace4e941792731
Björn Ramberg <bjorne@gah.se>
2014-06-03 22:22:56 +0200

    Set max input split size to 32 MB
    This will combine small files into one map task.

--- article_recommendations/lib/article_recommendations.rb
+++ article_recommendations/lib/article_recommendations.rb
@@ -9,6 +9,7 @@ Rubydoop.configure do |input_paths, output_path|
 
   job 'list urls per user id' do
     input input_paths, format: :combined_text
+    set 'mapreduce.input.fileinputformat.split.maxsize', 32 * (1024 ** 2)
     output url_list_per_user_id, format: :sequence_file
 
     mapper ArticleRecommendations::MapperUrlPerUserId
